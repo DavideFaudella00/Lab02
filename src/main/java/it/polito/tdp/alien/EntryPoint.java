8283babcc22e0c42f9ea94a7d712a8b6d51138df
david <david@172.22.102.255>
2022-03-16 16:52:46 +0100

    Finito

--- src/main/java/it/polito/tdp/alien/EntryPoint.java
+++ src/main/java/it/polito/tdp/alien/EntryPoint.java
@@ -2,37 +2,46 @@ package it.polito.tdp.alien;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import it.polito.tdp.model.Model;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class EntryPoint extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        scene.getRoot().setStyle("-fx-font-family: 'serif'");
+	@Override
+	public void start(Stage stage) throws Exception {
+		Model model = new Model();
+		FXMLController controller;
 
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
-    }
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+		Parent root = loader.load();
+
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add("/styles/Styles.css");
+		scene.getRoot().setStyle("-fx-font-family: 'Serif'");
+
+		controller = loader.getController();
+		controller.setModel(model);
+
+		stage.setTitle("JavaFX and Maven");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	/**
+	 * The main() method is ignored in correctly deployed JavaFX application. main()
+	 * serves only as fallback in case the application can not be launched through
+	 * deployment artifacts, e.g., in IDEs with limited FX support. NetBeans ignores
+	 * main().
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,24 +1,79 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	private Model model;
+	String regex = "[a-zA-Z]+";
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+	@FXML
+	private URL location;
 
-    }
+	@FXML
+	private Button btnClear;
+
+	@FXML
+	private Button btnTraduci;
+
+	@FXML
+	private TextArea txtArea;
+
+	@FXML
+	private TextField txtTraduci;
+
+	@FXML
+	void handleClear(ActionEvent event) {
+		txtTraduci.clear();
+		txtArea.clear();
+	}
+
+	@FXML
+	void handleTranslate(ActionEvent event) {
+		String celle[] = txtTraduci.getText().split(" ");
+		if (celle.length == 2) {
+			if (!celle[0].matches(regex) || !celle[1].matches(regex)) {
+				txtArea.setText("Errore, caratteri speciali inseriti");
+				return;
+			}
+			model.aggiungiAlDizionario(celle[0].toLowerCase(), celle[1].toLowerCase());
+			txtArea.setText("Alien:" + celle[0] + ", ITA: " + celle[1]);
+			txtTraduci.clear();
+
+		} else if (celle.length == 1) {
+			if (!celle[0].matches(regex)) {
+				txtArea.setText("Errore, caratteri speciali inseriti");
+				return;
+			}
+			txtArea.setText(model.traduci(celle[0].toLowerCase()));
+			txtTraduci.clear();
+		} else {
+			return;
+		}
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+
+	@FXML
+	void initialize() {
+		assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnTraduci != null : "fx:id=\"btnTraduci\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtTraduci != null : "fx:id=\"txtTraduci\" was not injected: check your FXML file 'Scene.fxml'.";
+
+	}
 
 }
